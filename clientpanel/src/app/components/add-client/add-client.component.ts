95660f2744371bd604c7198428b81476fb713d92
RustyJonas <rusty.letmaku28@gmail.com>
2018-04-17 15:17:01 +0200

    Add Client To Firestore

--- clientpanel/src/app/components/add-client/add-client.component.ts
+++ clientpanel/src/app/components/add-client/add-client.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { Client } from '../../models/Client';
-
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { ClientService } from '../../services/client.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-client',
@@ -18,10 +20,35 @@ export class AddClientComponent implements OnInit {
   }
 
   disableBalanceOnAdd : boolean = true;
-
-  constructor() { }
+  @ViewChild('clientForm') form: any;
+  constructor(
+    private flashMessage: FlashMessagesService,
+    private clientService: ClientService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
   }
 
+  onSubmit({value, valid}: {value: Client, valid: boolean}){
+    if(this.disableBalanceOnAdd){
+      value.balance = 0;
+    }
+
+    if(!valid){
+      // Show error
+      this.flashMessage.show('Please fill out the form correctly', {
+        cssClass: 'alert-danger', timeout: 4000
+      });
+    }else{
+      // Add new client
+      this.clientService.newClient(value);
+      // Show message
+      this.flashMessage.show('New client added', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+      // Redirect to dash
+      this.router.navigate(['/']);
+    }
+  }
 }
--- clientpanel/src/app/services/client.service.ts
+++ clientpanel/src/app/services/client.service.ts
@@ -29,4 +29,8 @@ export class ClientService {
       });
     return this.clients;
   }
+
+  newClient(client: Client){
+    this.clientsCollection.add(client);
+  }
 }
